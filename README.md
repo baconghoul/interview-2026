21b5688775703cac24647438344ae829add7f21b
liupeisheng002 <liupeisheng002@ke.com>
2025-08-12 21:28:42 +0800

    [紧急]两数之和

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# 2026年
-- algorithm包下面为leetcode题目解法
-- eight-legged包下面为八股文学习
+# 为2026年社招准备
+## 1.每日一题
+### 1.1 规则
+- 题库：热题100(https://leetcode.cn/studyplan/top-100-liked/)[链接]
+每天刷一道 leetcode 算法题，做完push到远程仓库
+### 1.2 
+## 面试题（JavaGuide） 
---
+++ 每日一题/两数之和/peipei01.java
@@ -0,0 +1,57 @@
+package 两数之和;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author liupeipei002@ke.com
+ * @date 2025/8/5 14:48
+ */
+public class peipei01 {
+
+    // todo: 第一题
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        // int[] res = new Solution().twoSum(nums, 9);
+        int[] res = new Solution1().twoSum(nums, 9);
+        System.out.println("res:[" + res[0] + "," + res[1] + "]");
+    }
+
+
+    static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int[] res = new int[2];
+            Map<Integer, Integer> numToIndex = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                numToIndex.put(nums[i], i);
+            }
+            for (int i = 0; i < nums.length; i++) {
+                int diff = target - nums[i];
+                Integer diffIndex = numToIndex.get(diff);
+                if (Objects.nonNull(diffIndex)) {
+                    res[0] = i;
+                    res[1] = diffIndex;
+                    return res;
+                }
+            }
+            return res;
+        }
+    }
+
+    static class Solution1 {
+        public int[] twoSum(int[] nums, int target) {
+            int[] res = new int[2];
+            Map<Integer, Integer> numToIndex = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                int diff = target - nums[i];
+                if (numToIndex.containsKey(diff)) {
+                    return new int[] {numToIndex.get(diff), i};
+                }
+                numToIndex.put(nums[i], i);
+            }
+            return res;
+        }
+    }
+}
